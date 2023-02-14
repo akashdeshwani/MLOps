152ea936418e84c5f35e20b6e57357ed95b391e9
Akash Deshwani <akashdeshwani@gmail.com>
2023-02-14 09:49:30 +0530

    Data and pytest

---
+++ house-prices-prediction/sandbox_pytest/mytest.py
@@ -0,0 +1,5 @@
+## function to test the function 
+
+def square(x):
+    return x*x
+
---
+++ house-prices-prediction/sandbox_pytest/test_mytest.py
@@ -0,0 +1,8 @@
+from mytest import square
+
+def test_square_gives_correct_result():
+    assert square(2) == 4
+    ## assert helps to check if the result is correct or not
+
+
+
